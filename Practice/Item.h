eb4a8963d26312ecaa95866456502d79c3e9ad74
FryingDutch <abbring_93@outlook.com>
2022-01-30 21:12:50 +0100

    update

---
+++ Practice/Item.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include "Entity.h"
+#include "Player.h"
+
+
+namespace EWF
+{
+	class Item : public Entity
+	{
+	private:
+		uint32_t health{ 0 };
+		uint32_t maxHealth{ 0 };
+		uint32_t attack{ 0 };
+		uint32_t defense{ 0 };
+
+		bool oneUse{ false };
+
+	public:
+		Item(std::string _ID, bool _oneUse) :
+			Entity(_ID), oneUse(_oneUse){}
+	};
+}
